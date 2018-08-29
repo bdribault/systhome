8fef27cff61792a79217fa97eabeca6f2d69ff3b
bdribault <benoit.dribault@gmail.com>
2018-08-29 16:52:43 +0200

    dim light : switch off button

--- gui/src/systhome/DimLight.js
+++ gui/src/systhome/DimLight.js
@@ -10,7 +10,8 @@ class DimLight extends React.Component {
 
         // This binding is necessary to make `this` work in the callback
         this.handleOnChange = this.handleOnChange.bind(this);
-        
+        this.handleSwitchOff = this.handleSwitchOff.bind(this);
+
         this.state = {
             value: parseInt(this.props.value["status"], 10),
             timeout: null
@@ -20,30 +21,42 @@ class DimLight extends React.Component {
     handleOnChange(event) {
         let value = event.target.value;
         // console.log("handleRangeChange", value)
-        
+
         if (this.state.timeout) {
             clearTimeout(this.state.timeout);
         }
 
         var timeout = setTimeout(
-            function() {
+            function () {
                 console.log("set value to ", this.state.value);
                 const device = this.props.value;
                 this.props.onChange(device.id, this.state.value);
             }
-            .bind(this),
+                .bind(this),
             200
         );
 
         this.setState({ value: value, timeout: timeout });
     }
 
+    handleSwitchOff(event) {
+        if (this.state.timeout) {
+            clearTimeout(this.state.timeout);
+        }
+        this.setState({ value: 0, timeout: null });
+        const device = this.props.value;
+        this.props.onChange(device.id, 0);
+    }
+
     render() {
         return (
-            <div>
-                <input className="slider has-output" step="1" min="0" max="100" value={this.state.value}
+            <div className="level">
+                <button className="delete is-small level-item"  onClick={this.handleSwitchOff}/>
+                <div style={{width:"10px"}}/>
+                <input className="slider has-output level-item" step="1" min="0" max="100" value={this.state.value}
                     type="range" onChange={this.handleOnChange} />
-                <output htmlFor="sliderWithValue">{this.state.value}</output>
+                <div style={{width:"10px"}}/>
+                <p className="level-item tag" htmlFor="sliderWithValue" style={{width:"30px"}}>{this.state.value}</p>
             </div>
         );
     }
